6e0a866402633da22842f0be6ae3613e723ba7b4
Radha Hariharan <rsradha@edvak.com>
2025-02-06 16:33:51 +0530

    Prime ng added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ehr-demo';
+
+  constructor(private primeng: PrimeNG) { }
+
+  ngOnInit(): void {
+    this.primeng.ripple.set(true);
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element && element.classList.toggle('dark-theme');
+  }
 }
